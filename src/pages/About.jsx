fd7c5a94bd513807bce6805aad13fb23a94c0eaa
Kanguli2001 <williamkanguli3@gmail.com>
2024-11-12 16:06:20 +0300

    create Pages and routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+
+
+export default function About() {
+  return (
+    <div>
+        <h1 className="text-4xl font-bold">About Page</h1>
+        <p className="text-2xl text-gray-700 font-semibold">This is the about page. It shows information about the company.</p>
+  
+    </div>
+  )
+}
